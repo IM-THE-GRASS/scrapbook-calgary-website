f3f3e5c977797444b29c5dcf50fd03a7fedc6fc0
Shuflduf <shuflduf@gmail.com>
2025-01-06 20:50:10 -0700

    change to vh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Background from "./background.svelte";
+</script>
+
+<Background />
+
+<div
+  class="flex justify-center flex-col w-full h-screen items-center absolute translate-y-24"
+>
+  <img src="/assets/logo.svg" alt="Logo" class="scale-[1.15]" />
+  <div class="flex justify-center items-center flex-col -translate-y-16">
+    <img
+      src="assets/white-strip.png"
+      alt="Ripped Paper"
+      class="scale-50 scale-x-[0.45]"
+    />
+    <img
+      src="assets/yellow-strip.svg"
+      alt="Ripped Paper"
+      class="scale-50 -translate-y-48 scale-x-[0.6]"
+    />
+  </div>
+</div>
---
+++ src/routes/background.svelte
@@ -0,0 +1,72 @@
+<img
+  src="/assets/cutting-mat.png"
+  alt="Cutting Mat"
+  class="cutting-mat absolute w-full"
+/>
+<img
+  src="/assets/paper-top.png"
+  alt="Paper Top"
+  class="paper-top absolute w-full"
+/>
+<img
+  src="/assets/trash-can.png"
+  alt="Trash Can"
+  class="trash-can absolute right-0"
+/>
+<img
+  src="/assets/ripped-paper.png"
+  alt="Paper Content"
+  class="paper-content absolute w-full"
+/>
+<img
+  src="/assets/ripped-paper-bottom.png"
+  alt="Paper Bottom"
+  class="paper-bottom absolute w-full"
+/>
+<img
+  src="/assets/bulletin.svg"
+  alt="Bulletin"
+  class="bulletin absolute w-full"
+/>
+<img
+  src="/assets/lined-paper.png"
+  alt="Lined Paper"
+  class="lined-paper absolute w-full"
+/>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: #337d78;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+  }
+
+  .cutting-mat {
+    top: 0;
+  }
+
+  .paper-top {
+    top: 90vh; /* Adjust the value as needed */
+  }
+
+  .trash-can {
+    top: 50vh; /* Adjust the value as needed */
+  }
+
+  .paper-content {
+    top: 140vh; /* Adjust the value as needed */
+  }
+
+  .paper-bottom {
+    top: 230vh; /* Adjust the value as needed */
+  }
+
+  .bulletin {
+    top: 330vh; /* Adjust the value as needed */
+  }
+
+  .lined-paper {
+    top: 467vh; /* Adjust the value as needed */
+  }
+</style>
