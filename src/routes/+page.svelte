4ce6f6f181234b162327524c3dea163194b6f1c9
IM-THE-GRASS <thegrassmanreplit@gmail.com>
2025-01-08 11:55:37 -0700

    athings with shadcn and things also the homepage maybe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,20 @@
 <script>
   import Background from "./background.svelte";
+  import { Button } from "$lib/components/ui/button";
 </script>
 
 <Background />
 
-<div
-  class="flex justify-center flex-col w-full h-screen items-center absolute translate-y-24"
->
-  <img src="/assets/logo.svg" alt="Logo" class="scale-[1.15]" />
-  <div class="flex justify-center items-center flex-col -translate-y-16">
-    <img
-      src="assets/white-strip.png"
-      alt="Ripped Paper"
-      class="scale-50 scale-x-[0.45]"
-    />
-    <img
-      src="assets/yellow-strip.svg"
-      alt="Ripped Paper"
-      class="scale-50 -translate-y-48 scale-x-[0.6]"
-    />
-    <h1 class="-translate-y-96 text-2xl">aaaaaaaaaaaaaaa</h1>
+<div class="flex justify-center flex-col w-full h-screen items-center absolute">
+  <img src="/assets/logo.svg" alt="Logo" class="scale-150  translate-y-500 mt-[15vh]"/>
+  <div class="flex justify-center flex-col w-full h-screen items-center relative gap-7">
+    <div class="text-4xl font-phantom font-extrabold text-center mx-16 text-white">
+      The <span class="text-red-400 bg-slate-900 rounded-md shadow-lg p-1">highschool hackathon</span>
+      where your wacky ideas
+    </div>
+    <div class="text-4xl font-phantom font-extrabold text-center mx-16 text-white">
+      become <span class="text-blue-400 bg-slate-900 rounded-md shadow-lg p-1">wonderful inventions.</span>
+    </div>
+    <Button variant="secondary" class="dark text-xl scale-150 text-white font-phantom font-bold hover:scale-105 transition-all duration-300 ">Sign up now</Button>
   </div>
 </div>
--- src/routes/background.svelte
+++ src/routes/background.svelte
@@ -3,36 +3,7 @@
   alt="Cutting Mat"
   class="cutting-mat absolute w-full"
 />
-<img
-  src="/assets/paper-top.png"
-  alt="Paper Top"
-  class="paper-top absolute w-full"
-/>
-<img
-  src="/assets/trash-can.png"
-  alt="Trash Can"
-  class="trash-can absolute right-0"
-/>
-<img
-  src="/assets/ripped-paper.png"
-  alt="Paper Content"
-  class="paper-content absolute w-full"
-/>
-<img
-  src="/assets/ripped-paper-bottom.png"
-  alt="Paper Bottom"
-  class="paper-bottom absolute w-full"
-/>
-<img
-  src="/assets/bulletin.svg"
-  alt="Bulletin"
-  class="bulletin absolute w-full"
-/>
-<img
-  src="/assets/lined-paper.png"
-  alt="Lined Paper"
-  class="lined-paper absolute w-full"
-/>
+
 
 <style lang="postcss">
   :global(html) {
@@ -43,27 +14,5 @@
     top: 0;
   }
 
-  .paper-top {
-    top: 90vh;
-  }
 
-  .trash-can {
-    top: 50vh;
-  }
-
-  .paper-content {
-    top: 140vh;
-  }
-
-  .paper-bottom {
-    top: 230vh;
-  }
-
-  .bulletin {
-    top: 330vh;
-  }
-
-  .lined-paper {
-    top: 467vh;
-  }
 </style>
