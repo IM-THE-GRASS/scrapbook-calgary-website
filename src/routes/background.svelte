0e55d475902e37ee8009153a1d9406ed4b3187c4
Shuflduf <shuflduf@gmail.com>
2025-01-13 01:39:22 -0700

    Add static adapter and update dependencies; fix asset paths and style formatting

--- src/routes/background.svelte
+++ src/routes/background.svelte
@@ -1,18 +1,15 @@
 <img
-  src="/assets/cutting-mat.png"
+  src="../assets/cutting-mat.png"
   alt="Cutting Mat"
   class="cutting-mat wavey w-full"
 />
 
-
 <style lang="postcss">
   :global(html) {
-    background-color: #0B111FFF;
+    background-color: #0b111fff;
   }
 
   .cutting-mat {
     top: 0;
   }
-
-
 </style>
